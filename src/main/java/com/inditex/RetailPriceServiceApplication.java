5c04dda00a2b05b34902298c0bbc03257af15e53
root <radax20003@gmail.com>
2024-08-06 14:02:32 +0200

    modified application

--- src/main/java/com/inditex/RetailPriceServiceApplication.java
+++ src/main/java/com/inditex/RetailPriceServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RetailPriceServiceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(RetailPriceServiceApplication.class, args);
 	}
 
